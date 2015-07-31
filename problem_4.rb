d3e6d38b42b378c2025ca0ba51b88316f17dfe5d
Sabron13 <kurt.schnelker@gmail.com>
2015-07-31 18:43:42 -0400

    wrote a method for identifying palindromes and a method that creates an array of palindromes that need checked to see if they are products of two-three digit numbers

---
+++ problem_4.rb
@@ -0,0 +1,28 @@
+def largest_palindrome(i)
+  placeholder = ''
+  i.times { placeholder += '9' }
+  
+  palindromes = []
+  (placeholder.to_i * placeholder.to_i).downto(100001).each do |num|
+    palindromes << num if palindrome?(num)
+  end
+
+  palindromes.each do |num|
+    
+  end
+
+end
+
+def palindrome?(num)
+  string_num = num.to_s
+  if string_num.length.even?
+    front = string_num[0..(string_num.length/2 - 1)]
+    back = string_num[string_num.length/2..-1]
+  else
+    front = string_num[0..(string_num.length.floor/2)]
+    back = string_num[string_num.length/2..-1]
+  end
+  front == back.reverse ? true : false
+end
+
+largest_palindrome(3)
