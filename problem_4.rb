7f973a3f687fca6f321d7a4aa3fcc8a7c7c9d9d4
Sabron13 <kurt.schnelker@gmail.com>
2015-08-03 17:44:17 -0400

    attempts to find all palindromes that are the product of two three-digit numbers.

--- problem_4.rb
+++ problem_4.rb
@@ -1,7 +1,17 @@
 def largest_palindrome_product(i)
-  
+  placeholder = ''
+  i.times { placeholder += '9' }
+  x = placeholder.to_i
+  y = placeholder.to_i
+  palindromes = []
 
-
+  (x..(0.1 * 10**i)).each do |i|
+    (y..(0.1 * 10**i)).each do |j|
+      product = i * j
+      palindromes << product if palindrome?(product)
+      p palindromes
+    end
+  end
 
 end
 
