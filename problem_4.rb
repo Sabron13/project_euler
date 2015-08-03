984176000f56127493f60231368c348ff4587a01
Sabron13 <kurt.schnelker@gmail.com>
2015-08-03 14:50:21 -0400

    rethinking strategy to identify largest palindrome

--- problem_4.rb
+++ problem_4.rb
@@ -1,25 +1,8 @@
 def largest_palindrome_product(i)
-  placeholder = ''
-  i.times { placeholder += '9' }
   
-  palindromes = []
-  (placeholder.to_i * placeholder.to_i).downto(100001).each do |num|
-    palindromes << num if palindrome?(num)
-  end
-  factors_sum = 0
-  largest_palindrome = []
-  while largest_palindrome.size < 1 do
-    palindromes.each do |num|
-      factors = []
-      while factors.size < 2 do
-        Math.sqrt(num).floor.downto(100).each do |factor|
-        factors << factor if num % factor == 0
-        end
-      end
-      factors_sum = factors.inject(:+) if factors.size == 2
-    end
-  end
-  p factors
+
+
+
 end
 
 
