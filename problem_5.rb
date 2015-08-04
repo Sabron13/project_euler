0122a5596f978019e4e89eb54aca388655abda38
Sabron13 <kurt.schnelker@gmail.com>
2015-08-04 16:00:57 -0400

    refactors for reusability

--- problem_5.rb
+++ problem_5.rb
@@ -2,7 +2,7 @@ def smallest_multiple(range)
   num = range.to_a[-1]
   range_size = range.to_a.size
   arr = []
-  while num < 240000000
+  loop do
     range.each do |i|
       num % i == 0 ? arr << 0 : break
     end
